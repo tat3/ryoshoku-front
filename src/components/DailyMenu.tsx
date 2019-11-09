fa1a3b9fddb53903b951b0a4c395ebaec1f2ab8d
tat3 <tat.github@gmail.com>
2019-11-09 20:41:01 +0900

    メニューを表示

---
+++ src/components/DailyMenu.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { withStyles, Theme } from '@material-ui/core/styles';
+import { WithStyles } from "@material-ui/core"
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+import { DailySchedule } from '../types'
+
+const styles = (theme: Theme) => ({
+  card: {
+    minwidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontsize: 14,
+  },
+  pos: {
+    marginbottom: 12,
+  },
+})
+
+interface Props extends WithStyles<typeof styles>{
+  menu: DailySchedule,
+}
+
+class DailyMenu extends React.Component<Props> {
+  render () {
+    const classes = this.props.classes
+    const menu = this.props.menu
+    return (
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
+            { menu.date }
+          </Typography>
+          <Typography variant="body2" component="p">
+            { menu.breakfast.exists ? menu.breakfast.content : 'お休み'}
+            <br />
+            { menu.dinner.exists ? menu.dinner.content : 'お休み'}
+          </Typography>
+        </CardContent>
+      </Card>
+    )
+  }
+}
+
+export default withStyles(styles)(DailyMenu)
---
+++ src/components/TopBar.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import RestaurantMenuIcon from '@material-ui/icons/RestaurantMenu'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}))
+
+export default function ButtonAppBar() {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <RestaurantMenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Ryoshoku.tk
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
