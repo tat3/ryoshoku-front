cc8f5513bf82faf12971b708b00f7256f013a81c
tat3 <tat.github@gmail.com>
2019-12-02 14:23:09 +0000

    topbarを修正

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
-import RestaurantMenuIcon from '@material-ui/icons/RestaurantMenu'
+import Drawer from '@material-ui/core/Drawer'
+import clsx from 'clsx'
+import List from '@material-ui/core/List'
+import Divider from '@material-ui/core/Divider'
+import MenuIcon from '@material-ui/icons/Menu'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import PlaceIcon from '@material-ui/icons/Place'
+
+const drawerWidth = 240
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -15,23 +27,84 @@ const useStyles = makeStyles(theme => ({
   title: {
     flexGrow: 1,
   },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  hide: {
+    display: 'none',
+  },
 }))
 
 export default function ButtonAppBar() {
   const classes = useStyles()
+  const theme = useTheme()
+  const [open, setOpen] = React.useState(false)
+
+  const handleDrawerOpen = () => {
+    setOpen(true)
+  }
+
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
+
+  const handleToDormitoryPage = () => {
+  }
+  const drawer = () => (
+    <Drawer
+      className={classes.drawer}
+      variant="persistent"
+      anchor="left"
+      open={open}
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+    >
+      <div className={classes.drawerHeader}>
+        <IconButton onClick={handleDrawerClose}>
+          {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+        </IconButton>
+      </div>
+      <Divider />
+      <List>
+        <ListItem button key={0} onClick={handleToDormitoryPage}>
+            <ListItemIcon><PlaceIcon /></ListItemIcon>
+            <ListItemText primary='寮の設定' />
+        </ListItem>
+      </List>
+    </Drawer>
+  )
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <RestaurantMenuIcon />
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             Ryoshoku
           </Typography>
         </Toolbar>
       </AppBar>
+      { drawer() }
     </div>
   )
 }
