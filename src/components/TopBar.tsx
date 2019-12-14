35d1eb7f86aa89bf9d3ca01c7fedd23c0c29018b
tat3 <tat.github@gmail.com>
2019-12-14 02:40:02 +0000

    更新機能を追加

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,25 +1,11 @@
 import React from 'react'
-import { Link, withRouter } from 'react-router-dom'
-import { RouteComponentProps } from 'react-router'
 import { Theme, withStyles, WithStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
-import Drawer from '@material-ui/core/Drawer'
 import clsx from 'clsx'
-import List from '@material-ui/core/List'
-import Divider from '@material-ui/core/Divider'
 import MenuIcon from '@material-ui/icons/Menu'
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
-import ChevronRightIcon from '@material-ui/icons/ChevronRight'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
-import PlaceIcon from '@material-ui/icons/Place'
-import HomeIcon from '@material-ui/icons/Home'
-
-import theme from '../theme'
 import { CSSProperties } from '@material-ui/core/styles/withStyles'
 import { useScrollTrigger, Slide } from '@material-ui/core'
 
@@ -28,7 +14,7 @@ const drawerWidth = 240
 const styles = (theme: Theme) => ({
   root: {
     flexGrow: 1,
-    position: "sticky",
+    position: 'sticky',
     height: 64,
   } as CSSProperties,
   menuButton: {
@@ -60,6 +46,8 @@ const styles = (theme: Theme) => ({
 })
 
 interface Props extends WithStyles<typeof styles> {
+  open: boolean,
+  handleMenuClicked(): void,
 }
 
 function HideOnScroll(props: any) {
@@ -67,84 +55,38 @@ function HideOnScroll(props: any) {
   const trigger = useScrollTrigger();
 
   return (
-    <Slide appear={false} direction="down" in={!trigger}>
+    <Slide appear={false} direction='down' in={!trigger}>
       {children}
     </Slide>
   );
 }
 
-class TopBar extends React.Component<Props & RouteComponentProps> {
-  state = {
-    open: false
-  }
-
-  setOpen = (open: boolean) => {
-    this.setState({open})
-  }
-
-  handleDrawerOpen = () => {
-    this.setOpen(true)
-  }
-
-  handleDrawerClose = () => {
-    this.setOpen(false)
-  }
-
+class TopBar extends React.Component<Props> {
   render () {
-    const { classes, location } = this.props
+    const { classes } = this.props
     return (
       <div className={classes.root}>
         <HideOnScroll { ...this.props }>
         <AppBar>
           <Toolbar>
             <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={this.handleDrawerOpen}
-              edge="start"
-              className={clsx(classes.menuButton, this.state.open && classes.hide)}
+              color='inherit'
+              aria-label='open drawer'
+              onClick={this.props.handleMenuClicked}
+              edge='start'
+              className={clsx(classes.menuButton, this.props.open && classes.hide)}
             >
               <MenuIcon />
             </IconButton>
-            <Typography variant="h6" className={classes.title}>
+            <Typography variant='h6' className={classes.title}>
               Ryoshoku
             </Typography>
           </Toolbar>
         </AppBar>
         </HideOnScroll>
-        <Drawer
-          className={classes.drawer}
-          variant="persistent"
-          anchor="left"
-          open={this.state.open}
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-        >
-          <div className={classes.drawerHeader}>
-            <IconButton onClick={this.handleDrawerClose}>
-              {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-            </IconButton>
-          </div>
-          <Divider />
-          <List>
-            { [
-                {text: 'ホーム', link: '/', icon: <HomeIcon />},
-                {text: '寮の設定', link: '/dormitory', icon: <PlaceIcon />},
-              ].filter(item => item.link !== location.pathname)
-                .map(({text, link, icon}, i) => (
-                <Link to={link} className={classes.link} onClick={this.handleDrawerClose} key={i}>
-                  <ListItem button>
-                    <ListItemIcon>{ icon }</ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItem>
-                </Link>
-              )) }
-          </List>
-        </Drawer>
       </div>
     )
   }
 }
 
-export default withRouter(withStyles(styles)(TopBar))
+export default withStyles(styles)(TopBar)
